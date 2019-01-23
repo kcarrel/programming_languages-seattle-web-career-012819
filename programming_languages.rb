49c2194ddf306200607675d62ce7ae4ee4df3b3f
Kathleen Carrel <katiecarrel@gmail.com>
2019-01-23 03:20:08 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -2,11 +2,10 @@ def reformat_languages(languages)
   reformatted = {}
   languages.each do |type, language|
     language.each do |name, data|
-      if reformatted[name] == NIL
-        reformatted[language] == data
-      end
-      reformatted[name][:style] = []
-      reformatted[name][:style] << style
+    reformatted[name] ||= data
+    reformatted[name][:style] = []
+    reformatted[name][:style] << type
     end
   end
+  reformatted
 end
